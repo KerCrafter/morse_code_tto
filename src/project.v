97f3b6339c15f6da2d7d0e088c1cfecfb70d55d2
yphoenixs <131599139+yphoenixs@users.noreply.github.com>
2025-09-23 14:36:38 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -18,11 +18,10 @@ module tt_um_morse (
 
   // All output pins must be assigned. If not used, assign to 0.
     assign uio_out[7:4]  = 'd0;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_in = 0;
   assign uio_oe  = 0;
 
     morse_top morse(.clk(clk),.rst(rst_n),.dot_inp(ui_in[0]),.dash_inp(ui_in[1]),.char_space_inp(ui_in[2]),.word_space_inp(ui_in[3]),.anode(uio_out[3:0]),.sout(uo_out));
   // List all unused inputs to prevent warnings
-    wire _unused = &{ena,clk,rst_n,1'b0};
+    wire _unused = &{ena,clk,rst_n,uio_in,1'b0};
 
 endmodule
