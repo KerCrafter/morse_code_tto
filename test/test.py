a4d5eb712acedca93338179ecae2bdcd8e1c898c
yphoenixs <131599139+yphoenixs@users.noreply.github.com>
2025-10-09 11:28:13 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -33,7 +33,7 @@ async def test_project(dut):
     dut.ui_in.value = 0b00000100  # char_space high
     await ClockCycles(dut.clk, 1)
     dut.ui_in.value = 0b00000000  # char_space low
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 3)
 
     # Wait for one clock cycle to see the output values
     #await ClockCycles(dut.clk, 1)
