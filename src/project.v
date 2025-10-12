ebd9ff4501d411ab063457f6af99e9c72a5b507a
yphoenixs <131599139+yphoenixs@users.noreply.github.com>
2025-10-12 10:54:21 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -20,7 +20,7 @@ module tt_um_morse (
     //assign uio_out[7:4]  = 'd0;  // Example: ou_out is the sum of ui_in and uio_in
   //assign uio_oe  = 0;
 
-    morse_top morse(.clk(clk),.rst(rst_n),.dot_inp(ui_in[0]),.dash_inp(ui_in[1]),.char_space_inp(ui_in[2]),.word_space_inp(ui_in[3]),.anode(uio_out[3:0]),.sout(uo_out));
+    morse_top morse(.clk(clk),.rst(rst_n),.dot_inp(ui_in[0]),.dash_inp(ui_in[1]),.char_space_inp(ui_in[2]),.word_space_inp(ui_in[3]),.sout(uo_out));
   // List all unused inputs to prevent warnings
     wire _unused = &{ena,clk,rst_n,uio_in,ui_in[7:4],1'b0};
 
