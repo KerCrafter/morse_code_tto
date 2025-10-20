9258630b0e35cfaf46f1cc86bbe9e7ca30da7ec8
yphoenixs <131599139+yphoenixs@users.noreply.github.com>
2025-10-20 11:46:37 +0530

    Update trans_fsm.v

--- src/trans_fsm.v
+++ src/trans_fsm.v
@@ -8,7 +8,6 @@ module trans_fsm(
     rst
 );
 
-//////////////  INPUTS   /////////////////
 input dot_inp;
 input dash_inp;
 input char_space_inp;
@@ -16,21 +15,16 @@ input word_space_inp;
 input clk;
 input rst;
 
-//////////// OUTPUTS /////////////////
 output reg [2:0] parallel_out;
 
-////////// parameters /////////
-// Main States
 parameter [3:0] s_idle       = 4'b0000;
 parameter [3:0] s_dot        = 4'b0001;
 parameter [3:0] s_dash       = 4'b0010;
 
-// 3-Unit Character Space States
 parameter [3:0] s_char_1     = 4'b0100;
 parameter [3:0] s_char_2     = 4'b0101;
 parameter [3:0] s_char_3     = 4'b0110;
 
-// 7-Unit Word Space States
 parameter [3:0] s_word_1     = 4'b1000;
 parameter [3:0] s_word_2     = 4'b1001;
 parameter [3:0] s_word_3     = 4'b1010;
@@ -39,11 +33,8 @@ parameter [3:0] s_word_5     = 4'b1100;
 parameter [3:0] s_word_6     = 4'b1101;
 parameter [3:0] s_word_7     = 4'b1110;
 
-// We need separate registers for current state and next state
 reg [3:0] state, next_state; 
 
-// PROCESS 1: Sequential Block for State Transitions
-// This block only describes the behavior of the state flip-flops.
 always@(posedge clk or negedge rst)
 begin
     if(!rst) begin
@@ -54,26 +45,23 @@ begin
     end
 end
 
-// PROCESS 2: Combinational Block for Next State and Output Logic
-// This block calculates the next state and outputs based on the current state and inputs.
 always@(*)
 begin
-    // Default assignments to prevent inferring latches
     next_state = s_idle;
-    parallel_out = 3'b000; // Default output for idle state
+    parallel_out = 3'b000; 
 
     case(state)
         s_idle:
         begin
-            parallel_out = 3'b000; // Inter-element space
+            parallel_out = 3'b000; 
             if (dot_inp)
                 next_state = s_dot;
             else if (dash_inp)
                 next_state = s_dash;
             else if (char_space_inp)
-                next_state = s_char_1; // Start the 3-cycle character space
+                next_state = s_char_1;
             else if (word_space_inp)
-                next_state = s_word_1; // Start the 7-cycle word space
+                next_state = s_word_1; 
             else
                 next_state = s_idle;
         end
